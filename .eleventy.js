4154632533e7a3dfdb05afb9fa575c4b12472751
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2020-11-25 22:55:21 +0700

    apply image transformation w/cloudinary

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const execSync = require('child_process').execSync
 const ampPlugin = require('@ampproject/eleventy-plugin-amp')
 const htmlmin = require('html-minifier')
+const cheerio = require('cheerio')
 
 module.exports = function(eleventyConfig) {
     //passthrough copy files
@@ -22,6 +23,25 @@ module.exports = function(eleventyConfig) {
     // build events
     eleventyConfig.on('beforeBuild', function() {
         execSync('npx gulp csscompile')
+    })
+
+    //replace image links
+    eleventyConfig.addTransform('replaceImgLinks', function(content, outputPath) {
+        if (process.env.ELEVENTY_ENV === 'prod') {
+            if (outputPath.endsWith('.html')) {
+                let $ = cheerio.load(content)
+                $('amp-img').each(function(){
+                    let imgUrl = $(this).attr('src')
+                    let imgWidth = $(this).attr('width')
+                    let imgHeight = $(this).attr('height')
+                    imgUrl = `https://res.cloudinary.com/dpc-pks-mb-ketapang/image/fetch/w_${imgWidth},h_${imgHeight},c_fill,f_auto/` + imgUrl
+                    $(this).attr('src', imgUrl)
+                })
+                return $.html()
+            }
+        } else {
+            return content
+        }
     })
 
     // AMP Plugin
