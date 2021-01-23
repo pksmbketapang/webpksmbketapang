dcbf4578b71b25fe2b0abf6f80f3f34356211220
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2021-01-23 11:39:42 +0700

    set up next.js with netlify

---
+++ postcss.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    plugins: [
+      'postcss-nested',
+      'tailwindcss',
+      [
+        'postcss-import-url',
+        {
+          modernBrowser: true
+        }
+      ],
+      'postcss-flexbugs-fixes',
+      [
+        'postcss-preset-env',
+        {
+          autoprefixer: {
+            flexbox: 'no-2009'
+          },
+          stage: 3,
+          features: {
+            'custom-properties': false
+          }
+        }
+      ]
+    ]
+  }
